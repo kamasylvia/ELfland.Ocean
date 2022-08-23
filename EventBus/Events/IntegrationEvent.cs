a5d1e81fddb731cc46a35043f32e39298d7264d8
Yue Zhang <zhangyue.dev@outlook.com>
2022-08-23 16:12:31 +0800

    Update EventBus

--- EventBus/Events/IntegrationEvent.cs
+++ EventBus/Events/IntegrationEvent.cs
@@ -2,9 +2,9 @@
 
 namespace Elfland.Ocean.EventBus.Events;
 
-public class IntegrationEvent
+public record class IntegrationEvent
 {
     public Guid? Id { get; private set; } = NewId.NextGuid();
 
-    public DateTime? CreationDate { get; private set; } = DateTime.UtcNow;
+    public DateTime? CreationDate { get; init; } = DateTime.UtcNow;
 }
